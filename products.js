ce06c97c412cb717f0528904a445ba0ad41f051e
Sina Moradian <sinacsharp@gmail.com>
2022-05-13 01:42:39 +0430

    Create products data

---
+++ products.js
@@ -0,0 +1,56 @@
+export const productsData = [
+    {
+        id: 1,
+        title: "PlayStation 5",
+        price: 499,
+        imageUrl: "./assets/images/ps5.jpg"
+    },
+    {
+        id: 2,
+        title: "Xbox Series X",
+        price: 499,
+        imageUrl: "./assets/images/xbox-series-x.jpg"
+    },
+    {
+        id: 3,
+        title: "Xbox series s",
+        price: 299,
+        imageUrl: "./assets/images/xbox-series-s.jpg"
+    },
+    {
+        id: 4,
+        title: "Nintendo Switch Lite",
+        price: 199.99,
+        imageUrl: "./assets/images/nintendo-switch-lite.jpg"
+    },
+    {
+        id: 5,
+        title: "Nintendo Switch OLED",
+        price: 349.99,
+        imageUrl: "./assets/images/nintendo-switch-oled.jpg"
+    },
+    {
+        id: 6,
+        title: "PlayStation 4 Pro 1TB",
+        price: 399,
+        imageUrl: "./assets/images/ps4-pro.jpg"
+    },
+    {
+        id: 7,
+        title: "PlayStation 4 slim 1TB",
+        price: 299.99,
+        imageUrl: "./assets/images/ps4.jpg"
+    },
+    {
+        id: 8,
+        title: "Nintendo Mario Edition",
+        price: 300,
+        imageUrl: "./assets/images/nintendo-switch-mario.jpg"
+    },
+    {
+        id: 9,
+        title: "Xbox One S All digital",
+        price: 299.99,
+        imageUrl: "./assets/images/xbox-one-s.jpg"
+    }
+]
