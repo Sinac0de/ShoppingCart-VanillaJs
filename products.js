a060c441f22bf6a002489bc559663acabfcb18ff
Sina Moradian <sinacsharp@gmail.com>
2022-05-13 02:11:05 +0430

    Added image alts data

--- products.js
+++ products.js
@@ -3,54 +3,63 @@ export const productsData = [
         id: 1,
         title: "PlayStation 5",
         price: 499,
-        imageUrl: "./assets/images/ps5.jpg"
+        imageUrl: "./assets/images/ps5.jpg",
+        imageAlt: "PlayStation 5"
     },
     {
         id: 2,
         title: "Xbox Series X",
         price: 499,
-        imageUrl: "./assets/images/xbox-series-x.jpg"
+        imageUrl: "./assets/images/xbox-series-x.jpg",
+        imageAlt: "xbox-series-x"
     },
     {
         id: 3,
         title: "Xbox series s",
         price: 299,
-        imageUrl: "./assets/images/xbox-series-s.jpg"
+        imageUrl: "./assets/images/xbox-series-s.jpg",
+        imageAlt: "xbox-series-s"
     },
     {
         id: 4,
         title: "Nintendo Switch Lite",
         price: 199.99,
-        imageUrl: "./assets/images/nintendo-switch-lite.jpg"
+        imageUrl: "./assets/images/nintendo-switch-lite.jpg",
+        imageAlt: "xbox-series-s"
     },
     {
         id: 5,
         title: "Nintendo Switch OLED",
         price: 349.99,
-        imageUrl: "./assets/images/nintendo-switch-oled.jpg"
+        imageUrl: "./assets/images/nintendo-switch-oled.jpg",
+        imageAlt: "Nintendo Switch Lite"
     },
     {
         id: 6,
         title: "PlayStation 4 Pro 1TB",
         price: 399,
-        imageUrl: "./assets/images/ps4-pro.jpg"
+        imageUrl: "./assets/images/ps4-pro.jpg",
+        imageAlt: "PlayStation 4 Pro"
     },
     {
         id: 7,
         title: "PlayStation 4 slim 1TB",
         price: 299.99,
-        imageUrl: "./assets/images/ps4.jpg"
+        imageUrl: "./assets/images/ps4.jpg",
+        imageAlt: "PlayStation 4 slim"
     },
     {
         id: 8,
         title: "Nintendo Mario Edition",
         price: 300,
-        imageUrl: "./assets/images/nintendo-switch-mario.jpg"
+        imageUrl: "./assets/images/nintendo-switch-mario.jpg",
+        imageAlt: "Nintendo switch mario edition"
     },
     {
         id: 9,
         title: "Xbox One S All digital",
         price: 299.99,
-        imageUrl: "./assets/images/xbox-one-s.jpg"
+        imageUrl: "./assets/images/xbox-one-s.jpg",
+        imageAlt: "Xbox one s"
     }
 ]
